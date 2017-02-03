a3b8a9431d5efb877f2dd1db834687d6eb58bf2b
Xiaowen <wenn009@gmail.com>
2017-02-02 22:40:10 -0500

    First commit

---
+++ PhotosViewController.swift
@@ -0,0 +1,114 @@
+//
+//  UserViewController.swift
+//  Tumblr
+//
+//  Created by Wenn Huang on 1/31/17.
+//  Copyright © 2017 Wenn Huang. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var posts:[NSDictionary]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 240
+        
+        
+        // Do any additional setup after loading the view.
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        //self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let posts = posts {
+            return posts.count
+        }
+        else {
+            return 0;
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell") as! photoCell
+        
+        let post = posts![indexPath.row]
+        
+        
+        //let timestamp = post["timestamp"] as? String
+        
+        if let photos = post.value(forKeyPath: "photos") as? [NSDictionary] {
+            // photos is NOT nil, go ahead and access element 0 and run the code in the curly braces
+            let imageUrlString = photos[0].value(forKeyPath: "original_size.url") as? String
+            
+            if let imageUrl = URL(string: imageUrlString!) {
+                // URL(string: imageUrlString!) is NOT nil, go ahead and unwrap it and assign it to imageUrl and run the code in the curly braces
+                cell.feedImage.setImageWith(imageUrl)
+                
+            } else {
+                // URL(string: imageUrlString!) is nil. Good thing we didn't try to unwrap it!
+            }
+
+        } else {
+            // photos is nil. Good thing we didn't try to unwrap it!
+        }
+        
+        return cell
+    }
+}
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ Tumblr/photoCell.swift
@@ -0,0 +1,27 @@
+//
+//  photoCell.swift
+//  Tumblr
+//
+//  Created by Wenn Huang on 2/2/17.
+//  Copyright © 2017 Wenn Huang. All rights reserved.
+//
+
+import UIKit
+
+class photoCell: UITableViewCell {
+
+    @IBOutlet var feedImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
