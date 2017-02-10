f412633ae10a79441b2702f23578781cbf866c7b
Xiaowen <wenn009@gmail.com>
2017-02-09 19:08:32 -0500

    Second commit

---
+++ Tumblr/InfiniteScrollActivityView.swift
@@ -0,0 +1,21 @@
+//
+//  InfiniteScrollActivityView.swift
+//  Tumblr
+//
+//  Created by Wenn Huang on 2/9/17.
+//  Copyright © 2017 Wenn Huang. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteScrollActivityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
